8f3e2dded77ecdce92ccc2e3e1836395d0f4d01c
Panji Hanum <83002940+panjihanumz@users.noreply.github.com>
2021-11-20 13:05:28 +0700

    INITIALIZE Spring Boot

---
+++ src/main/java/com/phanum/simpanpinjam/SimpanpinjamApplication.java
@@ -0,0 +1,13 @@
+package com.phanum.simpanpinjam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpanpinjamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpanpinjamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phanum/simpanpinjam/SimpanpinjamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phanum.simpanpinjam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpanpinjamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
